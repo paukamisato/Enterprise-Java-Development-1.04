c25467dc5b202dfb829bf84c640de97884e8a9fc
Paula Kamisato <paukamisato@gmail.com>
2022-04-19 20:32:39 +0200

    "final"

--- exercises/src/main/java/Exercises.java
+++ exercises/src/main/java/Exercises.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Exercises {
+public class Exercises {
+    public int largestAndSmallest(int[] numbers){
+        int largestValue = numbers[0];
+        int smallestValue = numbers[0];
+
+        if (numbers.length < 2){
+            System.out.println( "Invalid Input" );
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > largestValue) {
+                largestValue = numbers[i];
+            } else if (numbers[i] < smallestValue) {
+                smallestValue = numbers[i];
+            }
+        }
+        return (largestValue - smallestValue);
+    }
+
+    public String twoSmallest(int[] numbers){
+        String result = "";
+        int secondSmallest = Integer.MAX_VALUE;
+        int smallestValue = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < smallestValue) {
+                secondSmallest = smallestValue;
+                smallestValue = numbers[i];
+            } else if (numbers[i] < secondSmallest && numbers[i] != smallestValue){
+                secondSmallest = numbers[i];
+            }
+        }
+        result = "The smallest element is " + smallestValue + " and second Smallest element is " +secondSmallest;
+        return result;
+    }
+
+    public double equation(double x, double y){
+        double result = Math.pow(x,2)+Math.pow((((4*y)/5)-x), 2);
+        return result;
+    }
 }
--- exercises/src/main/java/Main.java
+++ exercises/src/main/java/Main.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Exercises exercises = new Exercises();
+
+        System.out.println("===Exercise1===");
+        int[] numbers = {2, 5, 8, 6, 3};
+        System.out.println("Numbers input: " + Arrays.toString(numbers));
+        int difference = exercises.largestAndSmallest( numbers );
+        System.out.println("The difference between the largest and smallest values is: "+ difference);
+
+        System.out.println("===Exercise2===");
+        int[] numbers1 = {10, 1, 6, 4, 9};
+        System.out.println("Numbers input: " + Arrays.toString(numbers1));
+        String twoSmallest = exercises.twoSmallest(numbers1);
+        System.out.println(twoSmallest);
+
+        System.out.println("===Exercise3===");
+        double x = 2;
+        double y = 3;
+        double result = exercises.equation( x, y );
+        System.out.println("The result is: " +result);
+    }
 }
